83ae1e554f32206915fdf7c87d73349777dff6ce
David Rivers <github.com@dpg.me>
2015-04-21 23:21:36 -0400

    Add function to form (except that dates are ignored)

--- app/scripts/main.coffee
+++ app/scripts/main.coffee
@@ -37,11 +37,25 @@ document.addEventListener "DOMContentLoaded", () ->
 		plotOptions:
 			series: { pointStart: startDate }
 		series: [
-			{ name: "Humans", color: "#000", data: humanSeries, pointInterval: 24 * 3600 * 1000 }
-			{ name: "Good Bots", color: "green", data: goodBotSeries, pointInterval: 24 * 3600 * 1000  }
-			{ name: "Bad Bots", color: "red", data: badBotSeries, pointInterval: 24 * 3600 * 1000 }
-			{ name: "Whitelist", color: "#ccc", data: whitelistSeries, pointInterval: 24 * 3600 * 1000  } ])
+			{ id: 1, name: "Humans", color: "#000", data: humanSeries, pointInterval: 24 * 3600 * 1000 }
+			{ id: 2, name: "Good Bots", color: "green", data: goodBotSeries, pointInterval: 24 * 3600 * 1000  }
+			{ id: 3, name: "Bad Bots", color: "red", data: badBotSeries, pointInterval: 24 * 3600 * 1000 }
+			{ id: 4, name: "Whitelisted", color: "#ccc", data: whitelistSeries, pointInterval: 24 * 3600 * 1000  } ])
 	
 	document.getElementById("form").addEventListener "submit", (evt) ->
 		evt.preventDefault()
-		console.log "nice submit!"
+
+		requestsValue = parseInt document.getElementsByName("requests")[0].value
+		requestsDate = dateToUTC document.getElementsByName("date")[0].value
+
+		seriesID
+		seriesRadios = document.getElementsByName "series"
+		i = 0
+		length = seriesRadios.length
+		while i < length
+			if seriesRadios[i].checked
+				seriesID = parseInt seriesRadios[i].value
+				break
+			i++
+
+		chart.get(seriesID).addPoint requestsValue if seriesID && requestsValue && requestsDate
--- dist/bundle.js
+++ dist/bundle.js
@@ -61,32 +61,51 @@ document.addEventListener("DOMContentLoaded", function() {
     },
     series: [
       {
+        id: 1,
         name: "Humans",
         color: "#000",
         data: humanSeries,
         pointInterval: 24 * 3600 * 1000
       }, {
+        id: 2,
         name: "Good Bots",
         color: "green",
         data: goodBotSeries,
         pointInterval: 24 * 3600 * 1000
       }, {
+        id: 3,
         name: "Bad Bots",
         color: "red",
         data: badBotSeries,
         pointInterval: 24 * 3600 * 1000
       }, {
-        name: "Whitelist",
+        id: 4,
+        name: "Whitelisted",
         color: "#ccc",
         data: whitelistSeries,
         pointInterval: 24 * 3600 * 1000
       }
     ]
   });
-  window.chart = chart;
   return document.getElementById("form").addEventListener("submit", function(evt) {
+    var i, length, requestsDate, requestsValue, seriesID, seriesRadios;
     evt.preventDefault();
-    return console.log("nice submit!");
+    requestsValue = parseInt(document.getElementsByName("requests")[0].value);
+    requestsDate = dateToUTC(document.getElementsByName("date")[0].value);
+    seriesID;
+    seriesRadios = document.getElementsByName("series");
+    i = 0;
+    length = seriesRadios.length;
+    while (i < length) {
+      if (seriesRadios[i].checked) {
+        seriesID = parseInt(seriesRadios[i].value);
+        break;
+      }
+      i++;
+    }
+    if (seriesID && requestsValue && requestsDate) {
+      return chart.get(seriesID).addPoint(requestsValue);
+    }
   });
 });
 
